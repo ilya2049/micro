177daeff1ce0b087cbbd336d90663983cb93b8d7
simba <simba@iageengineering.net>
2021-12-25 12:29:34 +0300

    refactor(hasher): use index term instead id term

--- internal/hasher/domain/hash/io.go
+++ internal/hasher/domain/hash/io.go
@@ -12,40 +12,40 @@ func NewInputs(strings []string) []Input {
 	return inputs
 }
 
-func NewInput(id int, value string) Input {
+func NewInput(index int, value string) Input {
 	return Input{
-		id:    id,
+		index: index,
 		value: []byte(value),
 	}
 }
 
 type Input struct {
-	id    int
+	index int
 	value []byte
 }
 
 func (in Input) String() string {
-	return fmt.Sprintf("{id: %d, value: %s}", in.id, in.value)
+	return fmt.Sprintf("{index: %d, value: %s}", in.index, in.value)
 }
 
 func (in Input) NewSHA3(value string) SHA3 {
-	return NewSHA3(in.id, value)
+	return NewSHA3(in.index, value)
 }
 
-func NewSHA3(id int, value string) SHA3 {
+func NewSHA3(index int, value string) SHA3 {
 	return SHA3{
-		id:    id,
+		index: index,
 		value: string(value),
 	}
 }
 
 type SHA3 struct {
-	id    int
+	index int
 	value string
 }
 
 func (sha3 SHA3) String() string {
-	return fmt.Sprintf("{id: %d, value: %s}", sha3.id, sha3.value)
+	return fmt.Sprintf("{index: %d, value: %s}", sha3.index, sha3.value)
 }
 
 type SHA3Hashes []SHA3
@@ -54,7 +54,7 @@ func (outputs SHA3Hashes) ToStrings() []string {
 	chunks := make([]string, len(outputs))
 
 	for _, output := range outputs {
-		chunks[output.id] = output.value
+		chunks[output.index] = output.value
 	}
 
 	return chunks
